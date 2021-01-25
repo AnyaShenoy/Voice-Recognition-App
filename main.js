eb9f8e5b61fa4d139cdddddd3d5f8f8b51a92f3c
AnyaShenoy <67114305+AnyaShenoy@users.noreply.github.com>
2021-01-25 15:45:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,17 +8,24 @@ recognition.onresult= function run(event){
     console.log(event);
     var content= event.results[0][0].transcript;
     console.log(content)
-    document.getElementById("textbox").innerHTML= content
-    speak()
+    document.getElementById("textbox").innerHTML= content;
+    if(content== "take my selfie"){
+        speak()
+        console.log("Taking My selfie")
+    }
 }
 
 function speak(){
     var synth= window.speechSynthesis
-    var speak_data= document.getElementById("textbox").value;
+    var speak_data= "Taking your selfie in 5 seconds"
     console.log("speak data= "+speak_data);
     var utter_this= new SpeechSynthesisUtterance(speak_data);
     synth.speak(utter_this);
     Webcam.attach(camera);
+    setTimeout(function(){
+        photo();
+        save();
+    }, 5000);
 }
  Webcam.set({
     width:360,
@@ -27,3 +34,16 @@ function speak(){
     png_quality: 90
  });
  camera= document.getElementById("camera");
+
+ function photo(){
+     Webcam.snap(function(data_uri){
+         document.getElementById("result").innerHTML="<img id='selfie' src= '"+data_uri+"'>";
+     })
+ }
+
+ function save(){
+     link= document.getElementById("link");
+     image= document.getElementById("selfie").src;
+     link.href= image;
+     link.click();
+ }
